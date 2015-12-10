aeb47acda4a55bb7581f13efdedbedf3d7c1c597
Nick Cotter <nick@extropy.net>
2015-12-10 16:52:19 +0000

    fiddling with doc type issues

--- src/healthkit_export/parse.clj
+++ src/healthkit_export/parse.clj
@@ -1,27 +1,9 @@
 (ns healthkit-export.parse
  (use [clj-xpath.core :only [$x $x:tag $x:text $x:attrs $x:attrs* $x:node]]))
 
-(def *some-xml*
-     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<books>
-  <book title=\"Some Guide To XML\">
-    <author>
-      <name>P.T. Xarnum</name>
-      <email>pt@x.m.l</email>
-    </author>
-    <description>
-      Simply the most comprehensive XML Book on the market today.
-    </description>
-  </book>
-  <book title=\"Some Guide To Functional Programming\">
-    <author>
-      <name>S. Hawking</name>
-      <email>universe@cambridge.ed.u</email>
-    </author>
-    <description>
-      This book is too smart for you, try 'Head first Quantum Mechanics for Dummies' instead.
-    </description>
-  </book>
-</books>")
+(def export-path "data/small_export.xml")
 
-(prn ($x:tag "/*" *some-xml*))
+(prn ($x:attrs* "//ExportDate" (slurp export-path) :value))
+
+
+
