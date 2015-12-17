5995514967bcf24a023c056f4455c60b70343fd1
Nick Cotter <nick@extropy.net>
2015-12-17 14:24:21 +0000

    basic reading and writing, with flaws

--- src/healthkit_export/core.clj
+++ src/healthkit_export/core.clj
@@ -1,7 +1,26 @@
 (ns healthkit-export.core
-  (:gen-class))
+  (:gen-class)
+  (use [clj-xpath.core]))
+
+(defn read-records
+  "returns sequence of record maps containing each Record's attributes"
+  [export-path]
+  (map #(-> % :attrs) ($x "//Record" (slurp export-path))))
+
+(defn to-csv-line
+  "maps record map to single line for writing"
+  [record]
+  (clojure.string/join "\t" [(record :type) (record :creationDate) (record :unit) (record :value)]))
+
+(defn write-csv
+  "writes a sequence of csv lines to the given filename"
+  [csv-lines output-filename]
+  (spit output-filename (clojure.string/join "\n" csv-lines))
+  )
+
+;; (write-csv (map to-csv-line (read-records "data/small_export.xml")) "health.csv")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (printf args))
--- src/healthkit_export/parse.clj
+++
@@ -1,9 +0,0 @@
-(ns healthkit-export.parse
- (use [clj-xpath.core :only [$x $x:tag $x:text $x:attrs $x:attrs* $x:node]]))
-
-(def export-path "data/small_export.xml")
-
-(prn ($x:attrs* "//ExportDate" (slurp export-path) :value))
-
-
-
